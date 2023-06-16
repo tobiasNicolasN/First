2343b1a02ae33705f49a6a055e7f70392a845952
tobiasNicolasN <135732822+tobiasNicolasN@users.noreply.github.com>
2023-06-16 13:20:47 -0300

    Update encriptadorDeTexto.py

--- encriptadorDeTexto.py
+++ encriptadorDeTexto.py
@@ -1,24 +1,47 @@
-#archivo = open("texto.txt", "a")
-#archivo.write("Prueba de guardado")
-#archivo.close()
-
 def encriptar(texto):
-    print("El texto a encriptar es: " + texto)
     textoFinal = ""
     for letra in texto:
         textoFinal += letra + "x"
-    print("El texto encriptado es: " + textoFinal)
+    return textoFinal
 
 def desencriptar(texto):
-    print("El texto a desencriptar es: " + texto)
     textoFinal = ""
     contador = 0
     for letra in texto:
         if contador % 2 == 0:
             textoFinal += letra
         contador += 1
-    print("El texto desencriptado es: " + textoFinal)
+    return textoFinal
 
-desencriptar("pxrxuxexbxax xdxex xexnxcxrxixpxtxaxdxox")
+def encriptarArchivo(rutaArchivo):
+    archivo = open(rutaArchivo, "r")
+    textoArchivo = archivo.read()
+    print("El texto a encriptar es: " + textoArchivo)
+    archivo.close()
+    archivo = open(rutaArchivo, "w")
+    print("El texto encriptado es: " + encriptar(textoArchivo))
+    archivo.write(encriptar(textoArchivo))
+    archivo.close()
 
-def encriptarArchivo():
+def desencriptarArchivo(rutaArchivo):
+    archivo = open(rutaArchivo, "r")
+    textoArchivo = archivo.read()
+    print("El texto a desencriptar es: " + textoArchivo)
+    archivo.close()
+    archivo = open(rutaArchivo, "w")
+    print("El texto desencriptado es: " + desencriptar(textoArchivo))
+    archivo.write(desencriptar(textoArchivo))
+    archivo.close()
+
+def usoPrograma():
+    print("Seleccione una opcion para archivo de texto:")
+    print("A - Para encriptar archivo")
+    print("B - Para desencriptar archivo")
+    uso = input("> ")
+    rutaArchivo = input("Ingrese la ruta de su archivo de texto: ")
+    if uso == "A":
+        encriptarArchivo(rutaArchivo)
+    else:
+        desencriptarArchivo(rutaArchivo)
+        
+usoPrograma()
