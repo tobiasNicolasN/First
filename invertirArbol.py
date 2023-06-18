96e006f5c3436e12fac6edbe62413110881b1d26
tobiasNicolasN <135732822+tobiasNicolasN@users.noreply.github.com>
2023-06-18 00:31:26 -0300

    Add files via upload

---
+++ invertirArbol.py
@@ -0,0 +1,57 @@
+class Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.left = None
+        self.right = None
+
+class Arbol:
+    def __init__(self,dato):
+        self.raiz = Nodo(dato)
+
+    def agregarRecursivo(self, nodo, dato):
+        if dato < nodo.dato:
+            if nodo.left is None:
+                nodo.left = Nodo(dato)
+            else:
+                self.agregarRecursivo(nodo.left, dato)
+        else:
+            if nodo.right is None:
+                nodo.right = Nodo(dato)
+            else:
+                self.agregarRecursivo(nodo.right, dato)
+
+    def agregar(self, dato):
+        self.agregarRecursivo(self.raiz, dato)
+
+    def preorderRecursivo(self, nodo, lista):
+        if nodo is not None:
+            lista.append(nodo.dato)
+            self.preorderRecursivo(nodo.left, lista)
+            self.preorderRecursivo(nodo.right, lista)
+
+    def arbolLista(self):
+        lista = []
+        self.preorderRecursivo(self.raiz, lista)
+        return lista
+
+    def invertirArbolRecursivo(self, nodo):
+        if nodo is None:
+            return None
+        nodo.left = self.invertirArbolRecursivo(nodo.right)
+        nodo.right = self.invertirArbolRecursivo(nodo.left)
+        return nodo
+
+    def invertirArbol(self):
+        self.raiz = self.invertirArbolRecursivo(self.raiz)
+
+arbolPrueba = Arbol(10)
+arbolPrueba.agregar(4)
+arbolPrueba.agregar(70)
+arbolPrueba.agregar(93)
+arbolPrueba.agregar(12)
+arbolPrueba.agregar(2)
+arbolPrueba.agregar(7)
+
+print(arbolPrueba.arbolLista())
+arbolPrueba.invertirArbol()
+print(arbolPrueba.arbolLista())
